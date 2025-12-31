92297ee5ecbb3332be575632ad5bf2cb63850054
彭宗文 ( A-Wen ) <101863812+AWEN-0719@users.noreply.github.com>
2025-12-31 14:27:46 +0800

    Update Exercise_2.c

--- C_Self-Study_Notes/Exercise_2.c
+++ C_Self-Study_Notes/Exercise_2.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int a = 1;  // 外層迴圈的被乘數
+    int b;      // 內層迴圈的乘數
+
+    while (a <= 9) {
+        b = 1;  // 每次外層開始，內層從 1 開始
+        while (b <= 9) {
+            printf("%2d x %2d = %3d\n", a, b, a * b);
+            b++;  // 內層迴圈加 1
+        }
+        printf("\n"); // 每一個乘法表空一行
+        a++;  // 外層迴圈加 1
+    }
+
+    return 0;
+}
